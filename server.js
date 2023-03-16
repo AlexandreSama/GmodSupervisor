eb526d6ca0c1864c8c1a5d9fefd809f55110125e
AlexandreSama <alexandrehainy12@gmail.com>
2023-03-16 19:25:28 +0000

    fix all this shit

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const session = require('express-session');
 const path = require('path');
 const app = express();
 const config = require('./config.json')
+const { Server } = require('@fabricio-191/valve-server-query');
+//const { io } = require("socket.io-client");
+//const socket = io();
 
 app.set('views', 'views');
 app.set('view engine', 'ejs');
@@ -36,23 +39,33 @@ app.get('/', (req, res) => {
     res.render('pages/login');
 });
 
-app.post('/auth', (req, res) => {
+app.post('/auth', async (req, res) => {
   // Insert Login Code Here
+  const server = await Server({
+	ip: '188.165.38.14',
+	port: 20020,
+	timeout: 3000,
+  });
   // Capture the input fields
   console.log(req.body)
 	let username = req.body.username;
 	let password = req.body.password;
 	// Ensure the input fields exists and are not empty
 	if (username && password) {
+		const players = await server.getPlayers();
+		req.session.playersIG = players.length.toString()
+		
 		// Execute SQL query that'll select the account from the database based on the specified username and password
-		connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		await connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
 			if (results.length > 0) {
+				console.log(results)
 				// Authenticate the user
 				req.session.loggedin = true;
 				req.session.username = username;
+				req.session.steamid = results[0].steamid
 				// Redirect to home page
 				res.redirect('/home');
 			} else {
@@ -76,7 +89,7 @@ app.post('/register_user', function(req, res){
 	let email = req.body.email
 	let steamid = req.body.steamid
 	// Ensure the input fields exists and are not empty
-	if (username && password && email && steamid) {
+	if (username && password && email && steamid && RegExp("/^[0-9]+$/").test(steamid) == true) {
 		// Execute SQL query that'll select the account from the database based on the specified username and password
 		connection.query('SELECT * FROM users WHERE email = ?', [email], function(error, results, fields) {
 			// If there is an issue with the query, output the error
@@ -90,13 +103,14 @@ app.post('/register_user', function(req, res){
 					if(result){
 						req.session.loggedin = true;
 						req.session.username = username;
+						req.session.steamid = steamid
 						res.redirect('/home');
 					}
 				})
 			}
 		});
 	} else {
-		res.send('Veuillez entrer un mot de passe et/ou un pseudonyme!');
+		res.send('Veuillez entrer un mot de passe et/ou un pseudonyme et/ou un steamid correct!');
 		res.end();
 	}
 })
@@ -116,6 +130,8 @@ app.get('/home', function(request, response) {
 		// Output username
 		response.render('pages/home', {
 			username: request.session.username,
+			steamid: request.session.steamid,
+			playersIG: request.session.playersIG
 		  })
 	} else {
 		// Not logged in
